21312a752778181785060b4e0122902696b35729
huypro <vuquochuy04@gmail.com>
2023-08-09 19:32:12 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import { createApp } from 'vue'
+import router from './router/index'
+import { Drawer, List, Menu, Button, message } from 'ant-design-vue';
+import App from './App.vue'
+
+import 'ant-design-vue/dist/reset.css';
+import 'bootstrap/dist/css/bootstrap-grid.min.css'
+import 'bootstrap/dist/css/bootstrap-utilities.min.css'
+
+const app = createApp(App);
+app.use(router);
+app.use(Button);
+app.use(Drawer);
+app.use(List);
+app.use(Menu);
+app.mount('#app');
+
+app.config.globalProperties.$message = message;
